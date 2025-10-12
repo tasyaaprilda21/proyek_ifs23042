bda68d43a5209244f3f6d2d4b2abf6f8b155536f
tasyamarbun <tasyaaprilda21@gmail.com>
2025-10-12 20:34:56 +0700

    gggg

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -16,6 +16,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   
+
   
   // Debug: Log request details
   console.log("API Request:", {
